a0239059cb2c7ca741040e1639e46bfa5d34701c
OPatrik0601 <patrikolasz0601@gmail.com>
2021-02-01 22:40:52 +0100

    Correct sizes for the loaded models
    Bigger models will be loaded correctly to the scene.
    + GameObject resize option changes

--- Assets/Scripts/AR/ImageRecognition.cs
+++ Assets/Scripts/AR/ImageRecognition.cs
@@ -21,11 +21,12 @@ public class ImageRecognition : MonoBehaviour
         }
     }
     public GameObject CreatedGameObject { get; private set; } = null;
+
     public void SetObject(GameObject loadedObject)
     {
         //the original, tracked GameObject rotates towards the camera by default, so we have to make a copy of it that doesn't rotate that way.
+        changeScale(loadedObject);
         loadedObject.gameObject.SetActive(false); //disable the original, tracked GO
-        loadedObject.transform.localScale = new Vector3(0.025f, 0.025f, 0.025f);
 
         GetComponent<ARTrackedImageManager>().trackedImagePrefab = loadedObject;
         GetComponent<ARTrackedImageManager>().trackedImagesChanged += (arg) =>
@@ -46,7 +47,6 @@ public class ImageRecognition : MonoBehaviour
             {
                 foreach (ARTrackedImage tracked in arg.updated)
                 {
-                    //for some reason Unity keeps attaching the ArTrackedImage to the GameObject, and it's making it rotate in a weird way sometimes
                     if(CreatedGameObject.GetComponent<ARTrackedImage>() != null)
                     {
                         Destroy(CreatedGameObject.GetComponent<ARTrackedImage>());
@@ -57,4 +57,43 @@ public class ImageRecognition : MonoBehaviour
             }
         };
     }
+
+    /// <summary>
+    /// Resizes the GameObject, so objects with bigger models will be visible as well
+    /// </summary>
+    /// <param name="obj">GameObject to resize</param>
+    private void changeScale(GameObject obj)
+    {
+        const float maxSize = 3.0f; //maximum size that a dimension can reach
+        const float baseScale = 0.025f; //the default scale that a GameObject should have if the dimension values are smaller than the maxSize
+        float biggestDifference = 0; //the biggest dimension that is higher than the maxSize
+        foreach (Renderer renderer in obj.GetComponentsInChildren<Renderer>()) //check all the renderers
+        {
+            Vector3 sizeCalculated = renderer.bounds.size; //the size of the model (so the max with maxSize=3 is (3.0, 3.0, 3.0))
+            Debug.Log($"The calculated size is {sizeCalculated}");
+            if (sizeCalculated.x > maxSize && sizeCalculated.x > biggestDifference)
+            {
+                biggestDifference = sizeCalculated.x;
+            }
+            if (sizeCalculated.y > maxSize && sizeCalculated.y > biggestDifference)
+            {
+                biggestDifference = sizeCalculated.y;
+            }
+            if (sizeCalculated.z > maxSize && sizeCalculated.z > biggestDifference)
+            {
+                biggestDifference = sizeCalculated.z;
+            }
+        }
+
+        Vector3 newValue = new Vector3(baseScale, baseScale, baseScale);
+        if (biggestDifference > 0)
+        {
+            float ratio = maxSize / biggestDifference; //the correction ratio for the new size
+            newValue *= ratio; //multiple the baseScale Vector with it
+            Debug.Log($"New scale: {newValue}, ratio: {ratio}");
+        }
+        ObjectScaling.Instance.ChangeSliderValues(newValue.x); //we could use the y, z coordinates as well, they're the same
+        obj.transform.localScale = newValue; //resize the GameObject
+    }
+
 }
--- Assets/Scripts/AR/ObjectScaling.cs
+++ Assets/Scripts/AR/ObjectScaling.cs
@@ -6,9 +6,21 @@ using UnityEngine.UI;
 
 public class ObjectScaling : MonoBehaviour
 {
+    private static ObjectScaling _instance = null;
+    public static ObjectScaling Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = Object.FindObjectOfType<ObjectScaling>();
+
+            return _instance;
+        }
+    }
+
     private Slider scaleSlider = null;
 
-    private void Start()
+    private void Awake()
     {
         scaleSlider = gameObject.GetComponent<Slider>();
         scaleSlider.onValueChanged.AddListener(onScaleSliderValueChanged);
@@ -18,6 +30,20 @@ public class ObjectScaling : MonoBehaviour
     {
         if(ImageRecognition.Instance.CreatedGameObject)
             ImageRecognition.Instance.CreatedGameObject.transform.localScale = new Vector3(value, value, value);
+    }
+
+    /// <summary>
+    /// Change the slider minvalue, maxvalue & current value
+    /// </summary>
+    /// <param name="normalSize">The current value, one dimension of the scale property</param>
+    public void ChangeSliderValues(float normalSize)
+    {
+        float maximum = normalSize * 1.9f; //maximum is 190%
+        float minimum = normalSize * 0.1f; //minimum is 10%
+        float currentValue = normalSize;
+        scaleSlider.minValue = minimum;
+        scaleSlider.maxValue = maximum;
+        scaleSlider.value = currentValue;
     }
 
 }
